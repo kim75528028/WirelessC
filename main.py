f04bd90d27ff4363f78fe319a00c38e3d5d6f829
kim75 <kim75528028@gmail.com>
2023-03-30 15:16:50 +0900

    무선통신

---
+++ main.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+m=0
+std=1
+min=-20
+max=20
+dx=0.001
+x=np.arange(min, max, dx)
+g1=1/(np.sqrt(2*np.pi)*std)
+g2=np.exp(-1*((x-m)**2)/(2*std**2))
+gauss=g1*g2
+plt.plot(x,gauss)
+plt.show()
