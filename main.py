f8358cfee4dc24888cc9e84f545f82e8a9244354
kim75 <kim75528028@gmail.com>
2023-03-30 15:40:26 +0900

    무선통신 5주차

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 m=0
-std=1
+std=2
 min=3
 max=20
 dx=0.001
@@ -14,5 +14,5 @@ m1=np.sum(gauss*dx) # 평균 구하기(평균=m)
 print(m1) #이론상 평균
 
 data_size=1000000
-data=np.random.randn(data_size) #가우스 랜덤 넘버 생성
+data=np.random.randn(data_size)*2  #가우스 랜덤 넘버 생성 , *2->표준편차를 2로 늘림
 print(np.sum(data>3)/data_size) #랜덤 생성 후 평균
